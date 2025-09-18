7d503568959c6a59965e9559f47ee38e265b5f82
Chunling Ji <chunlingji@Chunlings-KATE-MacBook-Pro.local>
2025-09-18 14:32:11 -0700

    add gitHUb link to lab1

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,8 +1,19 @@
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
+import "./index.css";
 export default function Lab2() {
     return (
-        <div id = "wd-lab2">
-            <h2>Lab 2</h2>
-            <h3>Intro to CSS</h3>
+        <div id="wd-lab2">
+            <h2>Lab 2 - Cascading Style Sheets</h2>
+            <h3>Styling with the STYLE attribute</h3>
+            <p style={{ backgroundColor: "blue", color: "white" }}>
+                Style attribute allows configuring look and feel
+                right on the element. Although it's very convenient
+                it is considered bad practice and you should avoid
+                using the style attribute
+            </p>
+            <ForegroundColors />
+            <BackgroundColors />
         </div>
-    )
+    );
 }
